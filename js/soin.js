1e81f45dcf7a9b1ef032d0370590f360d0a62ee6
Loïc POO-SING <130918150+loic-in-Lyon@users.noreply.github.com>
2026-04-08 15:11:56 +0200

    Add files via upload

--- js/soin.js
+++ js/soin.js
@@ -1,23 +1,29 @@
-const params = new URLSearchParams(window.location.search)
+const params = new URLSearchParams(window.location.search);
 
-const slug = params.get("slug")
+const slug = params.get("slug");
 
 fetch("data/soins.json")
-  .then(res => res.json())
-  .then(articles => {
+  .then((res) => res.json())
+  .then((articles) => {
+    const article = articles.find((a) => a.slug === slug);
 
-    const article = articles.find(a => a.slug === slug)
+    const head = document.querySelector("head");
+    // créer le title
+    const title = document.createElement("title");
+    // injecter le contenu
+    title.textContent = article.resume;
+    // ajouter dans le head
+    head.appendChild(title);
 
-    if (!article) return
+    if (!article) return;
 
-    document.getElementById("title").textContent = "Massage " + article.title
-    document.getElementById("duree").textContent = "Durée: " + article.duree
-    document.getElementById("content").textContent = article.content
-    article.benefits.forEach(element => {
-
+    document.getElementById("title").textContent = "Massage " + article.title;
+    document.getElementById("duree").textContent = "Durée: " + article.duree;
+    document.getElementById("content").textContent = article.content;
+    article.benefits.forEach((element) => {
       const li = document.createElement("li");
       li.className = "tag";
       li.textContent = element;
       document.getElementById("tag-wrapper").appendChild(li);
     });
-  })
+  });
